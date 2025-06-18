1acd985d3970f968cad3ec62b1bc8a06df4f2d70
gopes99 <gopes99@gmail.com>
2025-06-18 14:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
+from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify, send_from_directory
 import sqlite3
 import qrcode
 from io import BytesIO
