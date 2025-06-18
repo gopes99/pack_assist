0dcb62feede8b6ba99d4a8255ee3e82082ebc59a
gopes99 <gopes99@gmail.com>
2025-06-18 19:01:29 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -8,8 +8,24 @@ CREATE TABLE IF NOT EXISTS credentials (
     username TEXT PRIMARY KEY,
     credential_id BLOB NOT NULL,
     public_key BLOB NOT NULL
-)
+);
+-- credentials table
+CREATE TABLE credentials (
+    username TEXT PRIMARY KEY,
+    credential_id BLOB,
+    public_key BLOB
+);
+
+-- containers table
+CREATE TABLE containers (
+    id TEXT PRIMARY KEY,
+    content BLOB
+);
+
 """)
+
+
+
 
 print("Database created.")
 conn.commit()
